4d48447a5f4d7df2c7f3edc60a0dcb30989a63c4
Chris Zetter <zetter@users.noreply.github.com>
2018-11-17 14:36:39 +0000

    Remove 'crosswords are saved' notice
    I don't think this is helpful to display all the time

--- src/javascripts/crosswords/controls.js
+++ src/javascripts/crosswords/controls.js
@@ -109,9 +109,6 @@ class Controls extends Component {
             <div className="crossword__controls">
                 <div className="crossword__controls__clue">{controls.clue}</div>
                 <div className="crossword__controls__grid">{controls.grid}</div>
-                <div className="crossword__controls_autosave_label">
-                    Crosswords are saved automatically
-                </div>
             </div>
         );
     }
