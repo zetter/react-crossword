8760a86cc3425310ec17733bc8644885c1efda3a
Chris Zetter <zetter@users.noreply.github.com>
2018-11-22 15:47:53 +0000

    Simplify config
    We don't need to set an array since we're only providing one config.

--- webpack.config.examples.js
+++ webpack.config.examples.js
@@ -1,25 +1,23 @@
 const path = require('path');
 const baseConfig = require('./webpack.config');
 
-module.exports = [
-  {
-    entry: {
-      examples: path.join(
-        __dirname,
-        'examples',
-        'exampleCrossword.js',
-      ),
-    },
-    devServer: {
-      contentBase: path.join(__dirname, './examples/'),
-      port: 3000,
-    },
-    output: {
-      filename: 'examples.js',
-      path: path.join(__dirname, 'examples', 'lib'),
-    },
-    resolve: baseConfig.resolve,
-    resolveLoader: baseConfig.resolveLoader,
-    module: baseConfig.module,
+module.exports = {
+  entry: {
+    examples: path.join(
+      __dirname,
+      'examples',
+      'exampleCrossword.js',
+    ),
   },
-];
+  devServer: {
+    contentBase: path.join(__dirname, './examples/'),
+    port: 3000,
+  },
+  output: {
+    filename: 'examples.js',
+    path: path.join(__dirname, 'examples', 'lib'),
+  },
+  resolve: baseConfig.resolve,
+  resolveLoader: baseConfig.resolveLoader,
+  module: baseConfig.module,
+};
