fb266e2cac476cb4a235c837a71a80479486a9d8
Chris Zetter <zetter@users.noreply.github.com>
2018-11-22 15:30:42 +0000

    Remove duplication by importing some config from base

--- webpack.config.examples.js
+++ webpack.config.examples.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const webpack = require('webpack');
+const baseConfig = require('./webpack.config')
 
 module.exports = [
   {
@@ -18,42 +19,8 @@ module.exports = [
       filename: 'examples.js',
       path: path.join(__dirname, 'examples', 'lib'),
     },
-    resolve: {
-      modules: [
-        path.join(__dirname, 'src', 'javascripts'),
-        path.join(__dirname, 'src', 'stylesheets'),
-        'node_modules', // default location, but we're overiding above, so it needs to be explicit
-      ],
-      alias: {
-        svgs: path.join(__dirname, 'src', 'svgs'),
-      },
-    },
-    resolveLoader: {
-      modules: [
-        'node_modules',
-      ],
-    },
-    module: {
-      rules: [
-        {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: 'babel-loader',
-        },
-        {
-          test: /\.svg$/,
-          exclude: /node_modules/,
-          loader: 'svg-inline-loader',
-        },
-        {
-          test: /\.scss$/,
-          use: [
-            'style-loader', // creates style nodes from JS strings
-            'css-loader', // translates CSS into CommonJS
-            'sass-loader', // compiles Sass to CSS, using Node Sass by default
-          ],
-        },
-      ],
-    },
+    resolve: baseConfig.resolve,
+    resolveLoader: baseConfig.resolveLoader,
+    module: baseConfig.module,
   },
 ];
